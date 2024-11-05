25644a18326a176da8a7fd2ff3d8db61c6f23c1e
Akshay-Gengaje <Akshaysgengaje@gmail.com>
2024-11-05 12:30:49 +0530

    AppLayout imporved

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,7 +1,9 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "../components/Header";
 import { motion } from "framer-motion";
 const AppLayout = ({ toggleDarkMode, darkMode }) => {
+  const location = useLocation();
+  console.log("Current route:");
   return (
     <motion.div
       className="dark:bg-[#111111] min-h-screen bg-homepage bg-no-repeat bg-right"
@@ -16,7 +18,11 @@ const AppLayout = ({ toggleDarkMode, darkMode }) => {
         delay: 0.25,
       }}
     >
-      <div className="col-span-12 h-20 w-full top-0 left-0">
+      <div
+        className={`col-span-12 h-20 w-full ${
+          location.pathname == "/" ? "fixed top-0 left-0" : ""
+        }`}
+      >
         <Header darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
       </div>
       <div className="col-span-12 md:col-span-10 lg:col-span-11 dark:text-white text-black">
